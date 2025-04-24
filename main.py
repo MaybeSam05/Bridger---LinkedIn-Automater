7709323e974560829a5d2bdbe09db5e9ee6c8272
Samarth Verma <verma.samarth05@gmail.com>
2025-04-24 00:50:14 -0400

    screenshot function (not done)

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
     driver = webdriver.Chrome()
     driver.get("https://www.linkedin.com/login")
 
-    print("You have 60 seconds to log in manually...")
+    print("You have 30 seconds to log in manually...")
     time.sleep(30)
 
     with open("linkedin_cookies.pkl", "wb") as file:
@@ -15,6 +15,32 @@ def main():
     print("✅ Cookies saved to linkedin_cookies.pkl")
     driver.quit()
 
+    take_screenshot()
+
+def take_screenshot():
+    driver = webdriver.Chrome()
+
+    # Ask user for link to employee's LinkedIn profile
+    #employee_link = ask_employee_link()
+
+    employee_link = "https://www.linkedin.com/in/tavleen-singh2006/"
+
+    driver.get(employee_link)
+
+    with open("linkedin_cookies.pkl", "rb") as file:
+        cookies = pickle.load(file)
+        for cookie in cookies:
+            driver.add_cookie(cookie)
+
+    driver.refresh()
+    time.sleep(5)
+    driver.save_screenshot("screenshot.png")
+    print("✅ Screenshot saved as screenshot.png")
+    driver.quit()
+
+def ask_employee_link():
+    link = input("Enter the link to the employee's LinkedIn profile: ")
+    return link.strip()
 
 if __name__ == "__main__":
     main()
