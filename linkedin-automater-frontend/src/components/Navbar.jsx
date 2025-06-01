9eec71837b262b78fad962ecbc5de347cd9bf012
Samarth Verma <verma.samarth05@gmail.com>
2025-05-31 23:27:03 -0400

    navbar complete for now

--- linkedin-automater-frontend/src/components/Navbar.jsx
+++ linkedin-automater-frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import axios from 'axios';
 const banner = "py-4 px-6 md:px-12 flex justify-between items-center";
 const img = "object-contain";
 const getStartedButton = "text-white font-bold px-8 py-2 text-lg rounded-full hover:opacity-75 ";
-const logInButton = "text-white font-bold px-8 py-2 text-lg rounded-full hover:opacity-75";
+//const logInButton = "text-white font-bold px-8 py-2 text-lg rounded-full hover:opacity-75";
 
 const Navbar = () => {
   const navigate = useNavigate();  
@@ -40,9 +40,13 @@ const Navbar = () => {
 
       {/* Buttons on the right */}
       <div className="flex items-center gap-4">
+        { 
+        /*
         <button className= { logInButton } style = {{ backgroundColor: '#A3A3A3'}} >
           Log in
         </button>
+        */
+        }
         <button className= { getStartedButton } style={{ backgroundColor: '#0F689C' }} onClick={handleGetStarted}>
           Get Started Today!
         </button>
